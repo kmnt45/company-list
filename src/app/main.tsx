42b8f29e04192308c4bee4f076a2d152717cb7a2
kmnt46 <antonkmeta2005anna@gmail.com>
2024-12-01 02:35:57 +0300

    initial commit

---
+++ src/app/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+
+import { store } from '@/store';
+
+import { App } from './App.tsx';
+
+import 'normalize.css';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+);
